3a4286f91752134843aec0a5967ff1d4b52039f5
Xander Gonzaga <xandergonzaga@hotmail.com>
2025-05-09 23:28:15 +1000

    empty landing page with link to contacts

--- resonate-assessment-react/src/app/page.js
+++ resonate-assessment-react/src/app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex">
-      Empty page
+    <div className="flex flex-col w-screen items-center">
+      <p>Empty landing page for Resonate.</p>
+      <a href="/contacts" className="text-blue-500 underline">Contacts Page Link</a>
     </div>
   );
 }
